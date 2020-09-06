f0872f50a4ab7e44ba9759a71a321b0ca880566d
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-06 03:35:05 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import ExampleComponent from './ExampleComponent'
+import TestComponent from './TestComponent'
 class App extends Component {
 	render() {
 		// your code in the return statement below!
